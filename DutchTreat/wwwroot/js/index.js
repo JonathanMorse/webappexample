02e88ff1864d95cb1b3a954281d9c4e4b5cf81a8
Jonathan Morse <46463130+jonshere@users.noreply.github.com>
2020-05-27 21:03:20 -0600

    updated
    updated

--- DutchTreat/wwwroot/js/index.js
+++ DutchTreat/wwwroot/js/index.js
@@ -1,15 +1,29 @@
-﻿var x = 0;
-var s = "";
+﻿$(document).ready(function () {
 
-console.log("Hello Pluralsight");
+    var x = 0;
+    var s = "";
 
-var theForm = document.getElementById("theForm");
-theForm.hidden = true;
+    console.log("Hello Pluralsight");
 
-var button = document.getElementById("buyButton");
-button.addEventListener("click", function () {
-    console.log("Buying Item");
+
+
+    var theForm = $("#theForm");
+    theForm.hide();
+
+    var button = $("#buyButton");
+    button.on("click", function () {
+        console.log("Buying Item");
+    });
+
+    var productInfo = $(".product-props li");
+    productInfo.on("click", function () {
+        console.log("You clicked on " + $(this).text());
+    });
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+
+    $loginToggle.on("click", function () {
+        $popupForm.fadeToggle(1000);
+    })
 });
-
-var productInfo = document.getElementById("product-props");
-var listItems = productInfo.Item[0].children;
